f19b7012dd1867c9f05cf2617d6bdd4a7ad90981
Ethan Maddox <madethandox@gmail.com>
2026-09-14 21:37:50 +0330

    1.0.2

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const menuIcon = document.getElementById('menu-icon')
 const sideBar = document.getElementById('sidebar')
 menuIcon.addEventListener('click',()=> {
+    sideBar.style.transitionDuration = "0.5s" ;
     sideBar.style.transform = "translatex(0)" ;
 } )
 const closeIcon = document.getElementById('close-icon')
